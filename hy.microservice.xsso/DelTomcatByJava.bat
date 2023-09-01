d5e69ec825ad36fe2c46e0ffd78a61d7a41116d3
ZhengWei <HY.ZhengWei@qq.com>
2023-09-01 13:53:07 +0800

    1.0.6
    1. 升级：基础平台升级

--- hy.microservice.xsso/DelTomcatByJava.bat
+++ hy.microservice.xsso/DelTomcatByJava.bat
@@ -1,6 +1,6 @@
-del /S /Q C:\Software\apache-tomcat-9.0.44\webapps\hy.microservice.xsso\WEB-INF\classes\*.java
-del /S /Q C:\Software\apache-tomcat-9.0.44\webapps\hy.microservice.xsso\WEB-INF\layers.idx
-rd  /S /Q C:\Software\apache-tomcat-9.0.44\webapps\hy.microservice.xsso\WEB-INF\lib-provided
-rd  /S /Q C:\Software\apache-tomcat-9.0.44\webapps\hy.microservice.xsso\org
+del /S /Q %TOMCAT_HOME%\webapps\hy.microservice.xsso\WEB-INF\classes\*.java
+del /S /Q %TOMCAT_HOME%\webapps\hy.microservice.xsso\WEB-INF\layers.idx
+rd  /S /Q %TOMCAT_HOME%\webapps\hy.microservice.xsso\WEB-INF\lib-provided
+rd  /S /Q %TOMCAT_HOME%\webapps\hy.microservice.xsso\org
 
 pause
